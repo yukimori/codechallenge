cb846984f1012492dff881db658c1d78aef2cd4a
yukimori <yuharacomplex@gmail.com>
2020-10-03 09:12:16 +0900

    add algorithm: UnionFind

--- algorithm_datastructure/algorithm_datastructure_util/util_algorithm.py
+++ algorithm_datastructure/algorithm_datastructure_util/util_algorithm.py
@@ -6,6 +6,66 @@ from testfixtures import compare, Comparison as C
 from .util_test import time_measure
 
 
+# UnionFind
+"""
+UnionFindはN個のノードがいくつかのグループに分かれているときに
+1. 任意の２個のノードa, bが属するグループをつなげて１つのグループにする。
+2. 任意の２個のノードa, bが同じグループに属するのかを判定する
+という機能に特化したデータ構造。
+
+いずれもならし計算量log(N)で実装可能
+https://www.slideshare.net/chokudai/union-find-49066733/1
+https://qiita.com/Yosemat1/items/3b5f0ff6320ed63a2952
+"""
+class UnionFind:
+    """生成時には指定した数のグループができる。グループ番号を指定して操作することができる。
+    統合した場合は同じグループになる。
+    tree構造で表現する。
+    """
+    def __init__(self, n):
+        self.parents = list(range(n))
+
+    def _root(self, x):
+        """xの根を取得する
+
+        Args:
+            x ([type]): [description]
+
+        Returns:
+            int: [description]
+        """
+        if self.parents[x] == x:
+            # 根は親が自分自身
+            return x
+        else:
+            # 再帰して遡って根を見つける
+            root = self._root(self.parents[x])
+            return root
+
+    def is_united(self, x, y):
+        """2つのノードが同じグループにいるか判定する
+
+        Args:
+            x ([type]): [description]
+            y ([type]): [description]
+
+        Returns:
+            boolean: 同じグループにいるならTrue。違うならFalse
+        """
+        # 根が同じなら同じグループ
+        return self._root(x) == self._root(y)
+
+
+    def union(self, x, y):
+        """xが所属するグループをyが所属するグループに統合する
+
+        Args:
+            x ([type]): [description]
+            y ([type]): [description]
+        """
+        self.parents[self._root(y)] = self._root(x) 
+
+
 # フィボナッチ数列
 def fib(n):
     if n in (0, 1):
@@ -48,6 +108,15 @@ def isPalindrome(s):
 
 
 class TestAlgorithm(unittest.TestCase):
+
+    def test_unionfind(self, parameter_list=[5]):
+        for group_num in parameter_list:
+            uf = UnionFind(group_num)
+            uf.union(0, 3)
+            uf.union(1, 3)
+            self.assertTrue(uf.is_united(0, 1))
+
+
 
     def test_fib(self):
         self.assertEqual(1, fib(0))
